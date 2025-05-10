05958e17925c9ae2f67f297446a3e967ea926025
thiag <thiag@Thiago>
2025-05-10 15:43:36 -0300

    First commit!

---
+++ src/model/Task.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author thiag
+ */
+public class Task {
+    private String task;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private TaskStatus status;
+    
+    public Task(String task, LocalDate startDate, LocalDate endDate){
+        this.task = task;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        status = TaskStatus.DOING;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+    
+    
+    
+    
+}
